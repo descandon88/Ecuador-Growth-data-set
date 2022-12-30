c70416791edbf21765cccaba8a8031b59630fc38
David Escandón <david.escandon.v@gmail.com>
2022-12-30 19:35:52 +0000

    funcion prody

---
+++ funciones/prody.py
@@ -0,0 +1,48 @@
+
+# Export ‘Sophistication’
+# Recent research by Hausmann, Hwang, and Rodrik (2007) shows that the composition of a
+# country’s export basket has important implications for economic growth. Countries that have a
+# more ‘sophisticated’ export basket enjoy accelerated growth, while those that remain in less
+# sophisticated export sectors lag behind.
+
+def calc_prody(data,pais_col,prod_col,anio_col,export_col, gdp_per_capita_col):
+    
+    """
+      Función que calcula el valor de Prody
+      Xc: equals total exports by country c, 
+      Yc: equals GDP per capita of country c.
+
+    """
+    """
+      Where xvali,c,t equals exports of good i by country c in year t
+
+    """
+    print('En esta parte de la funcion se crea la combinación de producto-i, país-c y tiempo-t')
+    df_prody = pd.DataFrame(list(product(data[anio_col].unique(),data[pais_col].unique(),data[prod_col].unique())))
+    df_prody.columns=[anio_col,pais_col,prod_col]
+    print('Se procede a fusionar los datos')
+    df_prody = pd.merge(df_prody,data[[anio_col,pais_col,prod_col,export_col,gdp_per_capita_col]],how='left',on=[anio_col,pais_col,prod_col])
+    df_prody.loc[df_prody[export_col].isnull(),export_col] = 0
+
+    print('Se hace el calculo de RCEA')
+
+    df_prody['Xcpt'] = df_prody[export_col]
+    df_prody['Xct'] = df_prody.groupby([pais_col, anio_col])[export_col].transform(sum)
+    df_prody['Xpt'] = df_prody.groupby([prod_col, anio_col])[export_col].transform(sum)
+    df_prody['Xt'] = df_prody.groupby([anio_col])[export_col].transform('sum')
+    # df_prody['Yc'] = df_prody.groupby([pais_col, anio_col])[gdp_per_capita_col]
+    df_prody['Yc'] = df_prody[gdp_per_capita_col]
+    # yc_value = data.groupby(anio_col)[]
+    # df_xval = data.groupby(export_col).transform('sum')
+    df_prody['RCAcpt'] = (df_prody['Xcpt']/df_prody['Xct'])/(df_prody['Xpt']/df_prody['Xt']) #--> ecuación de las RCA
+    df_prody.loc[df_prody['RCAcpt'].isnull(),'RCAcpt'] = 0
+
+    print('Se hace el calculo de PRODY')
+
+    df_prody['PRODY'] = ((df_prody['Xcpt']/df_prody['Xct'])/(df_prody['Xpt']/df_prody['Xt']))*df_prody['Yc'] 
+
+    #retiro las propiedadades del dataframe 
+    df_prody.drop(['Xcpt','Xct','Xpt','Xt','Yc'],axis=1,inplace=True,errors='ignore')
+
+
+    return df_prody
